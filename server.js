f67388d6f8044f1101420c3c52b63d3b1c0f50c1
NITHESHMS97 <nitheshms97@gmail.com>
2018-12-05 15:58:34 +0530

    add project page

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ app.get('/about',(req,res)=>
   );
 })
 
+app.get('/projects',(req,res)=>
+{
+  res.render('projects.hbs')
+  {
+    pageTitle:'Project'
+  }
+})
 app.get('/bad',(req,res)=>
 {
   res.send({
